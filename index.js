374afbdec4edba936cc8fce5860e2d0441e57518
Ryan Burnette <ryan.burnette@gmail.com>
2019-08-26 19:09:01 -0400

    2.0.0

--- index.js
+++ index.js
@@ -1,76 +1,45 @@
 'use strict';
 
-var util = require('util');
-var request = util.promisify(require('@root/request'));
-var convert = require('xml-js');
-var fs = require('fs');
-var path = require('path');
+var axios = require('axios');
+var parseString = require('xml2js').parseString;
 
-function getMetar(airport) {
-    var url = new URL(
-        'https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=3&mostRecent=true'
-    );
-    url.searchParams.append('stationString', airport);
+var baseUrl =
+  'https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&hoursBeforeNow=3&mostRecent=true';
 
-    return request({ url: url.href })
-        .then(function (resp) {
-            if (
-                resp.statusCode == 200 &&
-                resp.headers['content-type'] == 'text/xml'
-            ) {
-                return resp.body;
-            }
-        })
-        .then(function (xml) {
-            return convert.xml2js(xml, { compact: true, spaces: 4 });
-        })
-        .then(function (metarObj) {
-            return metarObj.response.data.METAR;
-        })
-        .then(function (metar) {
-            return {
-                flightCategory: getFlightCat(metar),
-                obsTime: getObsTime(metar),
-                rawText: getRawText(metar)
-            };
-        })
-        .catch(function (err) {
-            throw err;
+module.exports = function(airport) {
+  return axios
+    .get(baseUrl + '&stationString=' + airport)
+    .then(function(resp) {
+      if (resp.status !== 200) {
+        throw new Error('got http status ' + resp.status);
+      }
+      if (resp.data.includes('<data num_results="0" />')) {
+        throw new Error('airport not found');
+      }
+      return resp.data;
+    })
+    .then(function(xmlString) {
+      return new Promise(function(resolve, reject) {
+        parseString(xmlString, {explicitArray: false}, function(err, result) {
+          if (err) {
+            return reject(err);
+          }
+          resolve(result);
         });
-}
-
-function getFlightCat(metar) {
-    return metar.flight_category._text;
-}
-
-function getObsTime(metar) {
-    return timeFormatter(new Date(metar.observation_time._text));
-}
-
-function getRawText(metar) {
-    var rawtext = removeRemarks(metar.raw_text._text);
-    return rawtext.substring(13, rawtext.length);
-    
-}
-
-function removeRemarks(rawtext) {
-    if (!rawtext.includes('RMK')){
-        return rawtext;
-    }
-    return rawtext.substring(0, rawtext.indexOf('RMK')).trim();
-}
-
-function timeFormatter(oldTime) {
-    var newTime = new Date(oldTime);
-    var minutes = '';
-    if (newTime.getMinutes() === 0) {
-        minutes = '00';
-    } else if (newTime.getMinutes() < 10) {
-        minutes = '0' + newTime.getMinutes().toString();
-    } else {
-        minutes = newTime.getMinutes().toString();
-    }
-    return newTime.getHours().toString() + ':' + minutes;
-}
-
-module.exports = getMetar;
+      });
+    })
+    .then(function(obj) {
+      try {
+        obj = obj.response.data.METAR;
+        obj.observation_time = new Date(obj.observation_time);
+        if (obj.sky_condition.length > 0) {
+          obj.sky_condition = obj.sky_condition.map(function(el) {
+            return el.$;
+          });
+        }
+      } catch (err) {
+        throw new Error('xml parse error');
+      }
+      return obj;
+    });
+};
